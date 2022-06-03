3b0a9508eb4d0aadfa6b70c7dfa0f43ba0a62aec
RomanRabinovich <roman.kliakhin@gmail.com>
2022-06-03 16:42:22 +0300

    first commit

---
+++ lib/widgets/auth/auth_widget.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+
+import '../../Theme/app_button_style.dart';
+
+class AuthWidget extends StatefulWidget {
+  const AuthWidget({Key? key}) : super(key: key);
+
+  @override
+  State<AuthWidget> createState() => _AuthWidgetState();
+}
+
+class _AuthWidgetState extends State<AuthWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login to your account'),
+      ),
+      body: ListView(
+        children: [
+          _HeaderWidget(),
+        ],
+      ),
+    );
+  }
+}
+
+class _HeaderWidget extends StatelessWidget {
+  const _HeaderWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = const TextStyle(
+      fontSize: 16,
+      color: Colors.black,
+    );
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 25),
+          _FormWidget(),
+          SizedBox(height: 25),
+          Text(
+            'In order to use the editing and rating capabilities of TMDB, as well as get personal recommendations you will need to login to your account. If you do not have an account, registering for an account is free and simple.',
+            style: textStyle,
+          ),
+          SizedBox(height: 5),
+          TextButton(
+              style: AppButtonStyle.linkButton,
+              onPressed: () {},
+              child: Text('Register')),
+          SizedBox(height: 25),
+          Text(
+            'If you signed up but did not get your verification email.',
+            style: textStyle,
+          ),
+          SizedBox(height: 5),
+          TextButton(
+              style: AppButtonStyle.linkButton,
+              onPressed: () {},
+              child: Text('verify email')),
+        ],
+      ),
+    );
+  }
+}
+
+class _FormWidget extends StatefulWidget {
+  const _FormWidget({Key? key}) : super(key: key);
+
+  @override
+  State<_FormWidget> createState() => __FormWidgetState();
+}
+
+class __FormWidgetState extends State<_FormWidget> {
+  final _loginTextController = TextEditingController();
+  final _passwordTextController = TextEditingController();
+  String? errorText = null;
+
+  void _auth() {
+    final login = _loginTextController.text;
+    final password = _passwordTextController.text;
+    if (login == 'admin' && password == 'admin') {
+      errorText = null;
+      print('open app');
+    } else {
+      errorText = 'Ne vernii login ili parol';
+    }
+    setState(() {});
+  }
+
+  void _resetPassword() {
+    print('reset password');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = const TextStyle(
+      fontSize: 16,
+      color: Color(0xFF212529),
+    );
+    final color = const Color(0xFF01B4E4);
+    final TextFieldDecorator = const InputDecoration(
+      border: OutlineInputBorder(),
+      contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      isCollapsed: true,
+    );
+    final errorText = this.errorText;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (errorText != null) ...[
+          Text(
+            errorText,
+            style: TextStyle(fontSize: 17, color: Colors.red),
+          ),
+          SizedBox(height: 20),
+        ],
+        Text('Username', style: textStyle),
+        SizedBox(height: 5),
+        TextField(
+          controller: _loginTextController,
+          decoration: TextFieldDecorator,
+        ),
+        SizedBox(height: 20),
+        Text('Password', style: textStyle),
+        SizedBox(height: 5),
+        TextField(
+          controller: _passwordTextController,
+          decoration: TextFieldDecorator,
+          obscureText: true,
+        ),
+        SizedBox(
+          height: 25,
+        ),
+        Row(
+          children: [
+            ElevatedButton(
+              onPressed: _auth,
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(color),
+                foregroundColor: MaterialStateProperty.all(Colors.white),
+                textStyle: MaterialStateProperty.all(
+                  TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+                ),
+                padding: MaterialStateProperty.all(EdgeInsets.symmetric(
+                  horizontal: 15,
+                  vertical: 8,
+                )),
+              ),
+              child: Text('Login'),
+            ),
+            SizedBox(width: 30),
+            TextButton(
+              onPressed: _resetPassword,
+              style: AppButtonStyle.linkButton,
+              child: Text('Resset password'),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
