fb832a0a4b45a6eef49f7cddda9f7fd1908d75d8
RomanRabinovich <roman.kliakhin@gmail.com>
2022-06-04 10:50:44 +0300

    bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_applazyload/Theme/app_colors.dart';
 import 'package:flutter_applazyload/widgets/auth/auth_widget.dart';
 import 'package:flutter_applazyload/widgets/main_screen/main_screen_widget.dart';
 
@@ -12,8 +13,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        appBarTheme:
-            AppBarTheme(backgroundColor: const Color.fromRGBO(3, 37, 65, 1)),
+        appBarTheme: AppBarTheme(backgroundColor: AppColors.mainDarkBlue),
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          backgroundColor: AppColors.mainDarkBlue,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+        ),
       ),
       routes: {
         '/auth': (context) => AuthWidget(),
--- lib/widgets/main_screen/main_screen_widget.dart
+++ lib/widgets/main_screen/main_screen_widget.dart
@@ -8,11 +8,53 @@ class MainScreenWidget extends StatefulWidget {
 }
 
 class _MainScreenWidgetState extends State<MainScreenWidget> {
+  int _selectedTab = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'News',
+    ),
+    Text(
+      'Movie',
+    ),
+    Text(
+      'Serial',
+    ),
+  ];
+
+  void onSelectTab(int index) {
+    if (_selectedTab == index) return;
+    setState(() {
+      _selectedTab = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('THDB'),
+      ),
+      body: Center(
+        child: _widgetOptions[_selectedTab],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedTab,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'News',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.movie_filter),
+            label: 'Movie',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.tv),
+            label: 'Serial',
+          ),
+        ],
+        onTap: onSelectTab,
       ),
     );
   }
