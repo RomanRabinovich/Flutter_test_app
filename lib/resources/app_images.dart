a1e76b6798fce47027bfb38648544110ea602726
RomanRabinovich <roman.kliakhin@gmail.com>
2022-06-07 15:41:33 +0300

    finished movie page

--- lib/resources/app_images.dart
+++ lib/resources/app_images.dart
@@ -3,6 +3,7 @@ part of 'resources.dart';
 class AppImages {
   AppImages._();
 
+  static const String actor = 'images/actor.jpg';
   static const String bear = 'images/bear.jpg';
   static const String topHeader = 'images/topHeader.jpg';
   static const String topHeaderSubImage = 'images/topHeaderSubImage.jpg';
--- test/app_images_test.dart
+++ test/app_images_test.dart
@@ -5,6 +5,7 @@ import 'package:test/test.dart';
 
 void main() {
   test('app_images assets test', () {
+    expect(File(AppImages.actor).existsSync(), true);
     expect(File(AppImages.bear).existsSync(), true);
     expect(File(AppImages.topHeader).existsSync(), true);
     expect(File(AppImages.topHeaderSubImage).existsSync(), true);
